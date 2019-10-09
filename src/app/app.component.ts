ae0817fcb44d4ab743d620300d82c53c67c6c35c
Techstriker <sales@techstriker.com>
2019-10-09 16:49:58 +0530

    added inital files

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Environment Project';
+  share() {
+    window.alert('The product has been static!');
+  }
+}
+
+
